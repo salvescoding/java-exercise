64544fe5c08f587a889f23bd0a9dfb7e989e51c7
Sergio Alves <salves2383@gmail.com>
2018-08-15 20:41:57 +0200

    two fer kata java

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,5 +1,12 @@
 class Twofer {
     String twofer(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String result = "";
+        if (name == null) {
+            result = "One for you, one for me.";
+        }
+        else {
+            result = String.format("One for %s, one for me.", name);
+        }
+        return result;
     }
 }
