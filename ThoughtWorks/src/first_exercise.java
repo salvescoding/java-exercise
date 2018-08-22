210178515660808554a41d7990c805d57e64c7ed
Sergio Alves <salves2383@gmail.com>
2018-08-22 17:31:47 +0100

    Completed isoscelesTriangle, drawDiamond

--- ThoughtWorks/src/first_exercise.java
+++ ThoughtWorks/src/first_exercise.java
@@ -12,7 +12,9 @@ public class first_exercise {
             drawHorizontalLine(numberEntered);
             drawVerticalLine(numberEntered);
             drawRightTriangle(numberEntered);
-            System.out.println("You have entered " + numberEntered);
+            isoscelesTriangle(numberEntered);
+            drawDiamond(numberEntered);
+            System.out.println("You have entered number: " + numberEntered);
         }
         else {
             System.out.println("Please enter an Integer");
@@ -20,15 +22,18 @@ public class first_exercise {
     }
 
     private static void printOne() {
+        System.out.println("Print one *");
         System.out.println("*");
     }
 
     private static void drawHorizontalLine(int n) {
         String repeated = new String(new char[n]).replace("\0", "*");
+        System.out.println("Draw Horizontal Line");
         System.out.println(repeated);
     }
 
     private static void drawVerticalLine(int n) {
+        System.out.println("Draw Vertical Line");
         while (n > 0) {
             System.out.println("*");
             n--;
@@ -37,11 +42,53 @@ public class first_exercise {
 
     private static void drawRightTriangle(int n) {
         int numberOfTimes = 1;
+        System.out.println("Draw Right Triangle");
         while (n > 0) {
             String repeated = new String(new char[numberOfTimes]).replace("\0", "*");
             System.out.println(repeated);
             n--;
             numberOfTimes++;
+        }
+    }
+
+    private static void isoscelesTriangle(int n) {
+        System.out.println("Isosceles Triangle");
+        buildTopDiamond(n);
+    }
+
+    private static void drawDiamond(int n) {
+        System.out.println("Draw Diamond centered");
+        buildTopDiamond(n);
+        buildBottomDiamond(n);
+    }
+
+    private static void buildTopDiamond(int n) {
+        int numberOfSpace = n - 1;
+        for(int i=1; i<=n; i++){
+
+            for(int j=numberOfSpace; j>0; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            numberOfSpace--;
+        }
+    }
+
+    private static void buildBottomDiamond(int n) {
+        int increaseSpace = 1;
+        for (int i = n-1; i > 0; i--){
+
+            for(int j=0; j<increaseSpace; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            increaseSpace++;
         }
     }
 
