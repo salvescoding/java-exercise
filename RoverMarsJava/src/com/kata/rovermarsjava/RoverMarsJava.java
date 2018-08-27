d8625e95a7ecefdc92732068b155b8ec1adeb231
Sergio Alves <salves2383@gmail.com>
2018-08-27 16:26:56 +0100

    Introduced txt file and methods to read file and store input in array instructions

--- RoverMarsJava/src/com/kata/rovermarsjava/RoverMarsJava.java
+++ RoverMarsJava/src/com/kata/rovermarsjava/RoverMarsJava.java
@@ -1,14 +1,38 @@
 package com.kata.rovermarsjava;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
 public class RoverMarsJava {
 
-    public int addNumber(int a, int b)
-    {
-        return a + b;
+    private static ArrayList instructions = new ArrayList();
+
+
+
+    public static void main(String[] args) {
+        openFile();
+
     }
 
-    public String fullName(String firstName, String lastName)
-    {
-        return firstName + " " + lastName;
+    public static void openFile() {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(new File("src/input.txt")));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                instructions.add(line);
+            }
+            System.out.println(instructions);
+
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+
     }
+
 }
