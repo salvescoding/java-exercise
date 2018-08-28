1c6a3c49464b5e3863d8a0b65cb4523ee80c953a
Sergio Alves <salves2383@gmail.com>
2018-08-28 20:21:48 +0100

    Created function to get rovers initial position and land rovers, also store rover in an array list

--- RoverMarsJava/src/com/kata/rovermarsjava/RoverMarsJava.java
+++ RoverMarsJava/src/com/kata/rovermarsjava/RoverMarsJava.java
@@ -5,19 +5,28 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class RoverMarsJava {
 
-    private static ArrayList instructions = new ArrayList();
+    private static List<String> instructions = new ArrayList<String>();
     private static Map map;
+    private static Coordinates coordinates;
+    private static Rover rover;
+    private static List<String> roversLandingPosition = new ArrayList<String>();
+    private static List<String> roversCommands = new ArrayList<String>();
+    private static List<Rover> rovers = new ArrayList<Rover>();
 
 
 
     public static void main(String[] args) {
         openFile();
         setMap();
+        getRoversInitialLocation();
+        landRovers();
     }
+
 
 
     private static void openFile() {
@@ -39,12 +48,34 @@ public class RoverMarsJava {
     }
 
     private static void setMap() {
-        String values = (String) instructions.get(0);
+        String values = instructions.get(0);
         String[] mapValues = values.split(" ");
         int x =  Integer.parseInt(mapValues[0]);
         int y =  Integer.parseInt(mapValues[1]);
         map = new Map(x, y);
     }
 
+    private static void getRoversInitialLocation() {
+        for (int i = 0; i < instructions.size(); i++) {
+            if (i % 2 != 0) {
+                roversLandingPosition.add(instructions.get(i));
+            }
+            else if ((i != 0) && (i % 2 == 0)) {
+                roversCommands.add(instructions.get(i));
+            }
+        }
+    }
+
+    private static void landRovers() {
+        for (String roverLanding : roversLandingPosition) {
+            String[] roverPosition = roverLanding.split(" ");
+            int x = Integer.parseInt(roverPosition[0]);
+            int y = Integer.parseInt(roverPosition[1]);
+            char d = roverLanding.charAt(4);
+            coordinates = new Coordinates(x, y, d, map);
+            rover = new Rover(coordinates);
+            rovers.add(rover);
+        }
+    }
 
 }
